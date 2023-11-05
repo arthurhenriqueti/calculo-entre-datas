64b243c9f90055af9b329894eba9365c9d535977
Arthur <arthurbleckrep2@gmail.com>
2023-11-04 23:57:25 -0300

    Incrementando a funcionalidade do calendário

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,80 @@ function pegarDatas() {
         resultadoElement.innerHTML = "Preencha ambos os campos de data!";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const calendar = document.getElementById("calendar");
+    
+    function generateCalendar(month, year) {
+        const daysInMonth = new Date(year, month + 1, 0).getDate();
+        const firstDay = new Date(year, month, 1).getDay();
+    
+        const monthNames = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    
+        const table = document.createElement("table");
+        const header = document.createElement("thead");
+        const body = document.createElement("tbody");
+    
+        const headerRow = document.createElement("tr");
+        const prevButton = document.createElement("th");
+        prevButton.textContent = "<";
+        prevButton.addEventListener("click", function() {
+            if (month === 0) {
+                generateCalendar(11, year - 1);
+            } else {
+                generateCalendar(month - 1, year);
+            }
+        });
+        const monthLabel = document.createElement("th");
+        monthLabel.setAttribute("colspan", "5");
+        monthLabel.textContent = monthNames[month] + " " + year;
+        const nextButton = document.createElement("th");
+        nextButton.textContent = ">";
+        nextButton.addEventListener("click", function() {
+            if (month === 11) {
+                generateCalendar(0, year + 1);
+            } else {
+                generateCalendar(month + 1, year);
+            }
+        });
+    
+        headerRow.appendChild(prevButton);
+        headerRow.appendChild(monthLabel);
+        headerRow.appendChild(nextButton);
+        header.appendChild(headerRow);
+        table.appendChild(header);
+    
+        const dayNames = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+        const daysRow = document.createElement("tr");
+        dayNames.forEach(day => {
+            const th = document.createElement("th");
+            th.textContent = day;
+            daysRow.appendChild(th);
+        });
+        body.appendChild(daysRow);
+    
+        let currentDay = 1;
+        for (let i = 0; i < 6; i++) {
+            const row = document.createElement("tr");
+            for (let j = 0; j < 7; j++) {
+                const cell = document.createElement("td");
+                if (i === 0 && j < firstDay) {
+                    cell.textContent = "";
+                } else if (currentDay > daysInMonth) {
+                    cell.textContent = "";
+                } else {
+                    cell.textContent = currentDay;
+                    currentDay++;
+                }
+                row.appendChild(cell);
+            }
+            body.appendChild(row);
+        }
+        table.appendChild(body);
+        calendar.innerHTML = "";
+        calendar.appendChild(table);
+    }
+
+    const currentDate = new Date();
+    generateCalendar(currentDate.getMonth(), currentDate.getFullYear());
+});
